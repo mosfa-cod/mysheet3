3a6f1eff50a70d67ef7fbb6bc56a644541cd6d8f
mostafa dahrog <111236159+mosfa-cod@users.noreply.github.com>
2026-06-13 20:48:51 +0100

    Refactor quiz application logic and UI elements

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ let seatNumber = "";
 const subjectName = "الورقة1"; 
 
 // ⚠️ ضع رابط تطبيق الويب الموحد الخاص بك (الإصدار 11) هنا:
-const webAppUrl = "https://script.google.com/macros/s/AKfycbyG2vRL6moDgnqL7uO4gUiUuI3Z1DI1f6OLSG_AIpAdLE8U80YME2bC3LFbHcRbosDX/exec"; 
+const webAppUrl = "https://script.google.com/macros/s/AKfycbyXiSyueUcPuKSIliAEHGMC30JfKi76wyZu_5pNEaqzHQLCVTi9AF9c8H3i5HhRWiyz/exec"; 
 
 const startScreen = document.getElementById('start-screen');
 const quizScreen = document.getElementById('quiz-screen');
