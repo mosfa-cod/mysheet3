67b84c5d8149553d5df7a759b75c39290a4e1c9a
mostafa dahrog <111236159+mosfa-cod@users.noreply.github.com>
2026-06-13 22:49:39 +0100

    Update sendQuizData function with new URL

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
  // الدالة المسؤولة عن إرسال بيانات الاختبار إلى جوجل شيت
 function sendQuizData() {
   // الرابط الخاص بنشر البرمجية (طريقة الحفظ الفعالة من Apps Script)
-  const url = "https://script.google.com/macros/s/AKfycbzL4fFuFpSb-Wc4k-Ekjn35EunnzP6tHeeOp4adHbGET26X9GeVZjwsrH89R-ta8TJF/exec";
+  const url = "https://script.google.com/macros/s/AKfycbxapWo7PGx9qWLNMAeUyCR6SjFz9l267ur1PmEUPa6SIzt2k1-agMAtM9maFlwm4ztJ/exec";
 
   // تجميع البيانات من حقول الإدخال في الموقع
   // تأكد أن المعرفات (IDs) تطابق عناصر صفحتك مثل input الخاص بالاسم ورقم الجلوس
